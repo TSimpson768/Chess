10e4a85d57a6320a6119ca5b6931b91b1d583b67
Thomas Simpson <thomas.simpson768@gmail.com>
2021-06-16 14:26:48 +0100

    Add owner to Piece class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,9 +1,17 @@
+require_relative 'constants'
 # Represents an individual piece. May need a load of children to
 # represent each piece. Also needs a pointer to its owner
 class Piece
-  def initialize
-    @symbol = '♔'
+  include Constants
+  def initialize(owner)
+    @owner = owner
+    @symbol = set_symbol
   end
-  attr_reader :symbol
+  attr_reader :symbol, :owner
 
+  def set_symbol
+    'W' if @owner.colour == WHITE
+
+    'B' if @owner.colour == BLACK
+  end
 end
