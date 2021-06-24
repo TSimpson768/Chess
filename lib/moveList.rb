a2f8ce99f21b4eb491544156208f0d160feb0337
Thomas Simpson <thomas.simpson768@gmail.com>
2021-06-24 16:00:35 +0100

    Complet movelist for non-sliding pieces

--- lib/moveList.rb
+++ lib/moveList.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Movelist starts with a posible move, and wether or not that move is
 # sliding (like a rook, bishop or queen)
 class MoveList
@@ -11,10 +12,10 @@ class MoveList
   # starting_position
   def valid_moves(starting_position, board, owner)
     moves = []
-    return moves if @sliding == false
+    next_pos = [starting_position[0] + @move[0], starting_position[1] + @move[1]]
+    return next_pos if @sliding == false && board.valid_pos?(next_pos, owner)
 
     loop do
-      next_pos = [starting_position[0] + @move[0], starting_position[1] + @move[1]]
       break if out_of_bounds?(next_pos)
 
       next_piece = board.locate_piece(next_pos)
@@ -24,6 +25,7 @@ class MoveList
       end
       moves.push(next_pos)
       starting_position = next_pos
+      next_pos = [starting_position[0] + @move[0], starting_position[1] + @move[1]]
     end
     moves
   end
--- spec/moveList_spec.rb
+++ spec/moveList_spec.rb
@@ -49,5 +49,48 @@ describe MoveList do
         expect(result).to eq(expected_result)
       end
     end
+
+    context 'For a non-sliding piece (pawn, knight, king)' do
+      subject(:non_sliding_movelist) { described_class.new([0, 1], false) }
+      it 'Returns only one move on an empty board' do
+        starting_position = [0, 0]
+        board = instance_double(Board)
+        owner = instance_double(Player)
+        allow(board).to receive(:locate_piece)
+        allow(board).to receive(:valid_pos?).and_return(true)
+        expected_result = [0, 1]
+        result = non_sliding_movelist.valid_moves(starting_position, board, owner)
+        expect(result).to eq(expected_result)
+      end
+
+      it 'Returns no moves if the target space is occupied by a friendly piece' do
+        starting_position = [0, 0]
+        board = instance_double(Board)
+        owner = instance_double(Player)
+        piece = instance_double(Piece)
+        allow(board).to receive(:locate_piece)
+        allow(board).to receive(:locate_piece).with([0, 1]).and_return(piece)
+        allow(board).to receive(:valid_pos?).and_return(true)
+        allow(board).to receive(:valid_pos?).with([0, 1], owner).and_return(false)
+        allow(piece).to receive(:owner).and_return(owner)
+        result = non_sliding_movelist.valid_moves(starting_position, board, owner)
+        expect(result).to eq([])
+      end
+
+      it 'Returns the move if target space is occupied by an enemy piece' do
+        starting_position = [0, 0]
+        board = instance_double(Board)
+        owner = instance_double(Player)
+        piece = instance_double(Piece)
+        opponent = instance_double(Player)
+        allow(board).to receive(:locate_piece)
+        allow(board).to receive(:locate_piece).with([0, 1]).and_return(piece)
+        allow(board).to receive(:valid_pos?).and_return(true)
+        allow(piece).to receive(:owner).and_return(opponent)
+        expected_result = [0, 1]
+        result = non_sliding_movelist.valid_moves(starting_position, board, owner)
+        expect(result).to eq(expected_result)
+      end
+    end
   end
 end
