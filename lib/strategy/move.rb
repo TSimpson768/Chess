50f86337d306c8c97ed33b400284dbaf10166edc
Thomas Simpson <thomas.simpson768@gmail.com>
2021-08-28 23:34:47 +0100

    Write a base move strategy

--- lib/strategy/move.rb
+++ lib/strategy/move.rb
@@ -1,9 +1,15 @@
+# Base strategy for moving pieces in a chessboard
 class Move
-  def initialize
-    
+  def make_move(move, board)
+    piece = locate_place(move[0], board).exit_place
+    locate_place(move[1], board).enter_place(piece)
+    board
   end
 
-  def make_move
-    
+  private
+
+  # Return the place object at pos in board array
+  def locate_place(pos, board)
+    board[pos[0]][pos[1]]
   end
 end
---
+++ spec/strategy_spec/move_spec.rb
@@ -0,0 +1,46 @@
+require_relative '../../lib/strategy/move'
+require 'pry'
+describe Move do
+  subject(:move) { described_class.new }
+  let(:board) { instance_double(Board) }
+  let(:empty_board) {
+    board = []
+    8.times do
+      row = []
+      8.times do
+        row.push(Place.new)
+      end
+      board.push(row)
+    end
+    board
+  }
+  let(:white) { instance_double(Player) }
+  let(:black) { instance_double(Player) }
+  let(:white_queen) { instance_double(Queen) }
+  let(:black_rook) { instance_double(Rook) }
+  before do
+    board.instance_variable_set(:@board, empty_board)
+    allow(black_rook).to receive(:move)
+    allow(white_queen).to receive(:move)
+  end
+  describe '#make_move' do
+    it 'correctly moves a piece to an empty space' do
+      empty_board[7][7].instance_variable_set(:@piece, black_rook)
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      expected_board[7][7].instance_variable_set(:@piece, nil)
+      expected_board[7][0].instance_variable_set(:@piece, black_rook)
+      result = move.make_move([[7, 7], [7, 0]], empty_board)
+      expect(result).to eq expected_board
+    end
+
+    it 'correctly executes a capture' do
+      empty_board[6][5].instance_variable_set(:@piece, black_rook)
+      empty_board[3][2].instance_variable_set(:@piece, white_queen)
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      expected_board[6][5].instance_variable_set(:@piece, white_queen)
+      expected_board[3][2].instance_variable_set(:@piece, nil)
+      result = move.make_move([[3, 2], [6, 5]], empty_board)
+      expect(result).to eq(empty_board)
+    end
+  end
+end
