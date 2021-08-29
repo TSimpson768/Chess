67c47b6db1d696d25915c4638ac9f0645305a976
Thomas Simpson <thomas.simpson768@gmail.com>
2021-08-29 12:45:46 +0100

    Wrote a castling strategy

--- lib/strategy/castle.rb
+++ lib/strategy/castle.rb
@@ -1,9 +1,15 @@
-class Castle
-  def initialize
-    
+# Movement strategy for castling
+require_relative 'move'
+class Castle < Move
+  def make_move(move, board)
+    moved_king_board = super(move, board)
+    super(rook_move(move[1]), moved_king_board)
   end
 
-  def make_move
-    
+  private
+
+  def rook_move(king_destination)
+    rank = king_destination[0]
+    king_destination[1] < 4 ? [[rank, 0], [rank, 3]] : [[rank, 7], [rank, 5]]
   end
 end
---
+++ spec/strategy_spec/castle_spec.rb
@@ -0,0 +1,72 @@
+require_relative '../../lib/strategy/castle'
+describe Castle do
+  subject(:castle) { described_class.new }
+  let(:empty_board) {
+    board = []
+    8.times do
+      row = []
+      8.times do
+        row.push(Place.new)
+      end
+      board.push(row)
+    end
+    board
+  }
+  let(:white) { instance_double(Player) }
+  let(:black) { instance_double(Player) }
+  let(:white_king) { instance_double(King) }
+  let(:black_king) { instance_double(King) }
+  let(:white_rook) { instance_double(Rook) }
+  let(:black_rook) { instance_double(Rook) }
+  before do
+    allow(white_king).to receive(:move)
+    allow(white_rook).to receive(:move)
+    allow(black_king).to receive(:move)
+    allow(black_rook).to receive(:move)
+  end
+  describe '#make_move' do
+    it 'Castles white kingside' do
+      empty_board[0][7].instance_variable_set(:@piece, white_rook)
+      empty_board[0][4].instance_variable_set(:@piece, white_king)
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      expected_board[0][6].instance_variable_set(:@piece, white_king)
+      expected_board[0][4].instance_variable_set(:@piece, nil)
+      expected_board[0][7].instance_variable_set(:@piece, nil)
+      expected_board[0][5].instance_variable_set(:@piece, white_rook)
+      result = castle.make_move([[0, 4], [0, 6]], empty_board)
+      expect(result).to eq expected_board
+    end
+
+    it 'Castles white queenside' do
+      empty_board[0][0].instance_variable_set(:@piece, white_rook)
+      empty_board[0][4].instance_variable_set(:@piece, white_king)
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      expected_board[0][2].instance_variable_set(:@piece, white_king)
+      expected_board[0][4].instance_variable_set(:@piece, nil)
+      expected_board[0][0].instance_variable_set(:@piece, nil)
+      expected_board[0][3].instance_variable_set(:@piece, white_rook)
+      result = castle.make_move([[0, 4], [0, 2]], empty_board)
+      expect(result).to eq expected_board
+    end
+
+    it 'castles black kingside' do
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      empty_board[7][7].instance_variable_set(:@piece, black_rook)
+      empty_board[7][4].instance_variable_set(:@piece, black_king)
+      expected_board[7][6].instance_variable_set(:@piece, black_king)
+      expected_board[7][5].instance_variable_set(:@piece, black_rook)
+      result = castle.make_move([[7, 4], [7, 6]], empty_board)
+      expect(result).to eq(expected_board)
+    end
+
+    it 'castles black queenside' do
+      expected_board = empty_board.map { |row| row.map(&:clone) }
+      empty_board[7][0].instance_variable_set(:@piece, black_rook)
+      empty_board[7][4].instance_variable_set(:@piece, black_king)
+      expected_board[7][2].instance_variable_set(:@piece, black_king)
+      expected_board[7][3].instance_variable_set(:@piece, black_rook)
+      result = castle.make_move([[7, 4], [7, 2]], empty_board)
+      expect(result).to eq(expected_board)
+    end
+  end
+end
