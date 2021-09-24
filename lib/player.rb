c8fd23990974243ccbce86dc62b5a51062ee9107
Thomas Simpson <thomas.simpson768@gmail.com>
2021-09-24 22:28:48 +0100

    Add == to player

--- lib/player.rb
+++ lib/player.rb
@@ -7,7 +7,12 @@ class Player
   def initialize(colour = WHITE, can_castle = true)
     @colour = colour
     @can_castle = can_castle
+    @check = false
   end
   attr_reader :colour
   attr_accessor :check, :can_castle
+
+  def ==(other)
+    @colour == other.colour && @check == other.check && @can_castle == other.can_castle
+  end
 end
